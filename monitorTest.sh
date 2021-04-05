8720b6b9774fa7c08cc2354022804eb9b939d279
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-04-04 19:58:19 -0500

    removed tailing whitespace

--- monitorTest.sh
+++ monitorTest.sh
@@ -1,17 +1,17 @@
 # MIT License
-# 
+#
 # Copyright (c) 2020 Brighton Sikarskie
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -27,7 +27,7 @@ hdmi() {
 	pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:hdmi-stereo-extra1+input:analog-stereo
 	xwallpaper --zoom $HOME/Pictures/Wallpapers/arch.png
 
-	while [ "$monitor" = "HDMI" ]; 
+	while [ "$monitor" = "HDMI" ];
 	do	
 		check
 	done
@@ -38,7 +38,7 @@ laptop() {
 	pactl set-card-profile alsa_card.pci-0000_00_1f.3 output:analog-stereo+input:analog-stereo
 	xwallpaper --zoom $HOME/Pictures/Wallpapers/arch.png
 
-	while [ "$monitor" = "laptop" ]; 
+	while [ "$monitor" = "laptop" ];
 	do	
 		check
 	done
